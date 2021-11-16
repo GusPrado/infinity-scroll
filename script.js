bd008038665d8ced493cbb6d1af491cea4278d66
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2021-11-15 21:10:46 -0300

    chore(script): Improve performance and code legibility

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
-const count = 10;
 const apiKey = '_Sg2mWoAFQ7FbA_De-n3tsqoh65ZQ47IloeYgMrCCbI';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let initialCount = 5;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
 let photosColletion = [];
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
+let isInitialLoad = true;
+
+const updateAPIURLWithNewCount = (pictureCount) => {
+  apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${pictureCount}`;
+};
 
 const imageLoaded = () => {
   imagesLoaded++;
@@ -48,6 +53,10 @@ const getPhotos = async () => {
     const response = await fetch(apiUrl);
     photosColletion = await response.json();
     displayPhotos();
+    if (isInitialLoad) {
+      updateAPIURLWithNewCount(30);
+      isInitialLoad = false;
+    }
   } catch (err) {
     console.log(err);
   }
